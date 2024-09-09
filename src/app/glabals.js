f0ba733f260720c593b1c0bf902124a6e1423fc0
klyse <Klaus.Pruenster@gmail.com>
2024-09-09 18:03:09 +0200

    update packages

--- src/app/glabals.js
+++ src/app/glabals.js
@@ -2,5 +2,4 @@ export const WorkStartDate = new Date(2014, 7, 1);
 
 export const WorkYears = () =>
   new Date().getFullYear() -
-  WorkStartDate.getFullYear() +
-  (new Date().getMonth() > WorkStartDate.getMonth() ? 1 : 0);
+  WorkStartDate.getFullYear();
