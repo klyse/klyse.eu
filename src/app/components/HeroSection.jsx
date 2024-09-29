ea42ab6aedb2f20bfd07433070fbe96adde665f9
klyse <Klaus.Pruenster@gmail.com>
2024-09-29 13:50:33 +0200

    new hero image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -64,13 +64,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="absolute top-0 left-0 w-full h-full object-cover"
+              width={400} // Make sure width matches div size
+              height={400} // Make sure height matches div size
             />
           </div>
         </motion.div>
