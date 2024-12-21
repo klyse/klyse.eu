ba2cfc2615cd503e530b944999829776418ae968
klyse <Klaus.Pruenster@gmail.com>
2024-12-21 20:14:36 +0100

    remove speed insights as it breaks next

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +29,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
-      <SpeedInsights />
     </html>
   );
 }
